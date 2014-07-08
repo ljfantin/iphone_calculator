b700724927e4977f6550840a2d3398bf9ab1a078
Leandro Fantin <lfantin@mac-lfantin.local>
2014-07-08 09:54:50 -0300

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Leandro Fantin on 08/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Leandro Fantin on 08/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
